b1f7cb96ed22b6e9b2b674132b9ae24bcc66bc11
Overloader <Overloader@tutanota.com>
2022-11-03 09:29:09 +0100

    Some Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,88 +1,15 @@
 pub mod menutree;
 
-use std::fs::File;
-use std::path::Path;
 use petgraph::graph::UnGraph;
 use serde::{Serialize, Deserialize};
 use crate::menutree::MenuTree;
 
 /// Represents a usable deltav map
-///
-/// Can be deserialized from a json value like this:
-/// ```json
-/// {
-///   "menu_tree": {
-///     "MiddleNode": {
-///       "name": "Category1",
-///       "children": [
-///         {
-///           "MiddleNode": {
-///             "name": "Category2",
-///             "children": [
-///               {
-///                 "EndNode": {
-///                   "name": "Node1",
-///                   "id": 0
-///                 }
-///               },
-///               {
-///                 "EndNode": {
-///                   "name": "Node2",
-///                   "id": 1
-///                 }
-///               }
-///             ]
-///           }
-///         },
-///         {
-///           "EndNode": {
-///             "name": "Node3",
-///             "id": 2
-///           }
-///         },
-///         {
-///           "EndNode": {
-///             "name": "Node4",
-///             "id": 3
-///           }
-///         }
-///       ]
-///     }
-///   },
-///   "graph": {
-///     "nodes": [
-///       0,
-///       1,
-///       2,
-///       3
-///     ],
-///     "node_holes": [],
-///     "edge_property": "undirected",
-///     "edges": [
-///       [
-///         0,
-///         1,
-///         100
-///       ],
-///       [
-///         1,
-///         2,
-///         400
-///       ],
-///       [
-///         2,
-///         3,
-///         900
-///       ]
-///     ]
-///   }
-/// }
-/// ```
 #[derive(Deserialize)]
 #[cfg_attr(test, derive(Debug, Serialize))]
 pub struct DeltavMap {
     menu_tree: MenuTree,
-    graph: UnGraph<usize, i32>
+    graph: UnGraph<String, i32>
 }
 
 impl DeltavMap {
@@ -90,7 +17,7 @@ impl DeltavMap {
         &self.menu_tree
     }
 
-    fn get_graph(&self) -> &UnGraph<usize, i32> {
+    fn get_graph(&self) -> &UnGraph<String, i32> {
         &self.graph
     }
 }
@@ -105,32 +32,34 @@ impl PartialEq for DeltavMap {
 #[cfg(test)]
 mod tests {
     use std::fs::File;
-    use petgraph::Graph;
     use petgraph::graph::UnGraph;
-    use crate::menutree::tests::get_test_tree;
     use crate::DeltavMap;
+    use crate::MenuTree::{EndNode, MiddleNode};
 
-    fn get_test_graph() -> UnGraph<usize, i32>{
-        let mut graph: UnGraph<usize, i32> = UnGraph::new_undirected();
-
-        let node1 = graph.add_node(0);
-        let node2 = graph.add_node(1);
-        let node3 = graph.add_node(2);
-        let node4 = graph.add_node(3);
-
-
-        graph.add_edge(node1, node2, 100);
-        graph.add_edge(node2, node3, 400);
-        graph.add_edge(node3, node4, 900);
-
-        graph
-    }
 
     fn get_test_map() -> DeltavMap {
-        let menu_tree = get_test_tree();
-        let graph = get_test_graph();
+        let mut graph: UnGraph<String, i32> = UnGraph::new_undirected();
 
-        DeltavMap {menu_tree, graph}
+        let menu_tree = MiddleNode { name: "Category1".to_owned(), children: vec![
+            MiddleNode { name: "Category2".to_owned(), children: vec![
+                EndNode { name: String::from("Node1"), index: graph.add_node(String::from("Node1"))},
+                EndNode { name: String::from("Node2"), index: graph.add_node(String::from("Node2"))},
+            ] },
+            EndNode { name: String::from("Node3"), index: graph.add_node(String::from("Node3")) },
+            EndNode { name: String::from("Node4"), index: graph.add_node(String::from("Node4")) },
+        ] };
+
+        graph.add_edge(menu_tree["Node1"].get_index().clone(),
+                       menu_tree["Node2"].get_index().clone(),
+                       900);
+        graph.add_edge(menu_tree["Node2"].get_index().clone(),
+                       menu_tree["Node3"].get_index().clone(),
+                       80);
+        graph.add_edge(menu_tree["Node3"].get_index().clone(),
+                       menu_tree["Node4"].get_index().clone(),
+                       50);
+
+        DeltavMap { menu_tree, graph }
     }
 
     #[test]
