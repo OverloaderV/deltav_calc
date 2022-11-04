f702847eb18bb493aa1320682cc1a0c3f95712d4
Overloader <Overloader@tutanota.com>
2022-11-04 18:01:32 +0100

    Just a little test that workspaces work

--- deltav_calc_bin/src/main.rs
+++ deltav_calc_bin/src/main.rs
@@ -1,5 +1,6 @@
-use deltav_calc_lib;
+use deltav_calc;
 
 fn main() {
-    println!("Hello, world!");
+    let map = deltav_calc::DeltavMap::get_stock();
+    println!("{}", map.calculate_delta_v("Low Kerbin Orbit (80km)", "Low Mun Orbit (14km)").unwrap().unwrap())
 }
