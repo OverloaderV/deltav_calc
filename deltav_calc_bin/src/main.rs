5bfe8513e92347ab43cf2e9468886490261d7a60
Overloader <Overloader@tutanota.com>
2022-11-04 21:46:47 +0100

    Comments

--- deltav_calc_bin/src/main.rs
+++ deltav_calc_bin/src/main.rs
@@ -15,12 +15,15 @@ fn main() {
     app.run();
 }
 
+// Builds the ui
 fn build_ui(app: &Application) {
+    // The window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Deltav Calculator")
         .build();
 
+    // Build the menu tree
     let map = deltav_calc::DeltavMap::get_stock();
     let tree = map.get_menu_tree();
     window.set_child(Some(&build_tree(tree)));
@@ -28,24 +31,29 @@ fn build_ui(app: &Application) {
     window.present();
 }
 
+// Build the menu tree
 fn build_tree(tree: &MenuTree) -> Widget {
     return match tree {
         MenuTree::MiddleNode { children, .. } => {
+            // A box to hold the children
             let vbox = gtk::Box::builder()
                 .orientation(Vertical)
                 .margin_start(20)
                 .build();
 
+            // Recursively add children
             for child in children {
                 BoxExt::append(&vbox, &build_tree(child));
             }
 
+            // Create the expander
             Widget::from(Expander::builder()
                 .label(tree.get_name())
                 .child(&vbox)
                 .build())
         }
         MenuTree::EndNode { .. } => {
+            // Create a button for the EndNode
             let button = Button::builder()
                 .label(tree.get_name())
                 .width_request(100)
@@ -57,6 +65,7 @@ fn build_tree(tree: &MenuTree) -> Widget {
     }
 }
 
+// On click for when something in the menu tree is clicked
 fn tree_clicked(button: &Button) {
     println!("{}", button.label().unwrap());
 }
